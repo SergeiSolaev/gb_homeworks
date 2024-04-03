acb6cf62e95419e9828c30c8a13bd5f412979ada
SergeiSolaev <sergeisolaev@yandex.ru>
2024-04-03 22:48:41 +0400

    Add JavaScript_basic Homework_1

---
+++ 14_JavaScript_basics/Homework_1/script.js
@@ -0,0 +1,8 @@
+let tempCelsium = 20;
+let tempFarenheit = (9 / 5) * tempCelsium + 32;
+console.log('tempCelsium :>> ', tempCelsium);
+console.log('tempFarenheit :>> ', tempFarenheit);
+
+let name = 'Sergei';
+let admin = name;
+console.log(admin);
