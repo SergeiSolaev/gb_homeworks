adc0d7db73da001b556ac48334de4b4ae9c2ef52
SergeiSolaev <sergeisolaev@yandex.ru>
2024-06-04 22:48:14 +0400

    Add VUE.js Homework_4

---
+++ 19. Фреймворк VUE.js/Homework_4/app/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <MainPage/>
+  <BlogPage/>
+  <BlogDetailsPage/>
+</template>
+
+<script>
+import MainPage from './pages/MainPage.vue'
+import BlogPage from './pages/BlogPage.vue'
+import BlogDetailsPage from './pages/BlogDetailsPage.vue'
+
+export default {
+  name: '',
+  components: {
+    MainPage,
+    BlogPage,
+    BlogDetailsPage,
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
